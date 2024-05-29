35643ae812f127a221bdce92d58153149173426e
Leonardo Alvarenga <leonardoalvarengam@gmail.com>
2024-05-29 01:35:30 -0300

    aula sobre operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem" + "Java";
+
+        System.out.println(nomeCompleto);
+
+        int a,b;
+
+        a = 7;
+        b = 7;
+
+        int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+    }
+}
+ 
