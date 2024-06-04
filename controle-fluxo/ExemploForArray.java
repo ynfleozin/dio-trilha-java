56fc6f1fa28ce4f29658398f166ca24cd536d906
Leonardo Alvarenga <leonardoalvarengam@gmail.com>
2024-06-04 15:53:17 -0300

    aulas sobre estruturas de repetição

---
+++ controle-fluxo/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"FELIPE", "LEONARDO", "MARIA"};
+
+        for (int x=0; x <= alunos.length; x++){
+            System.out.println("O aluno do índice x=" + x + " é " + alunos[x] );
+        }
+    }
+}
