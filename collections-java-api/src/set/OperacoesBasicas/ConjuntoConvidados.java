d51c462802dec2e5c01490948db69dcdcc13dd17
Leonardo Alvarenga <leonardoalvarengam@gmail.com>
2024-06-18 14:27:23 -0300

    Aulas sobre opercoes basicas com set interface

---
+++ collections-java-api/src/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,46 @@
+package set.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados(){
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c: convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        conjuntoConvidados.adicionarConvidado("Rafael", 12345);
+        conjuntoConvidados.adicionarConvidado("Luiz", 12347);
+        conjuntoConvidados.adicionarConvidado("Maria", 12349);
+
+        conjuntoConvidados.exibirConvidados();
+        System.out.println(conjuntoConvidados.contarConvidados());
+    }
+}
