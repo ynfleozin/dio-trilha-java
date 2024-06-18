2c921fe683d385d0589d1b282ff9a83e30cf887f
Leonardo Alvarenga <leonardoalvarengam@gmail.com>
2024-06-18 14:57:01 -0300

    Aulas sobre pesquisa com set interface

---
+++ collections-java-api/src/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,52 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos(){
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos(){
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for(Contato c: contatoSet){
+            if(c.getNome().startsWith(nome)){
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for(Contato c: contatoSet){
+            if(c.getNome().equalsIgnoreCase(nome)){
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Leonardo", 999452138);
+        agendaContatos.adicionarContato("Marcelo", 999452147);
+        agendaContatos.adicionarContato("Paulo", 999452487);
+
+        System.out.println(agendaContatos.pesquisarPorNome("Leonardo"));
+    }
+}
